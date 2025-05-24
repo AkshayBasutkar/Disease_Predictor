c8e7f8382f1ea556aeb77b6cbe219b7858909e55
Akshay Basutkar <100553716+AkshayBasutkar@users.noreply.github.com>
2025-05-24 23:49:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ page = st.sidebar.selectbox("Go to", ["Disease Predictor", "Diet Recommendations
 
 if page == "Disease Predictor":
     # Import and run the disease predictor page code here
-    import pages.disease_predictor as predictor
+    import pages.Disease_Predictor as predictor
     predictor.app()
 
 elif page == "Diet Recommendations":
     # Import and run the diet recommendations page code here
-    import pages.diet_recommendations as diet_page
+    import pages.Diet_Recommendations as diet_page
     diet_page.app()
